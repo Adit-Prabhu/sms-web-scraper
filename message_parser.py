eba44f5f069458f0dfb15937fce14e3f70863a79
aditprbhu <adprabh2@asu.edu>
2023-11-03 22:11:09 -0700

    Parses additional strings and clusters unparsed strings

--- message_parser.py
+++ message_parser.py
@@ -20,6 +20,33 @@ with open('sms_messages.txt', 'r') as infile, open('parsed_companies.txt', 'w') 
         r'para (\w+) es',
         r'use this OTP(\d+)for (\w+)\.',
         r'Tap to reset your (\w+) password:',
+        r'【阿里巴巴】您正在登录验证，验证码\d+，',
+        r'您正在进行短信登录，验证码\d+，',
+        r'注册验证码 \d+，5分钟内有效，请勿告知他人',
+        r'您本次验证码为:\d+,如非本人操作，请联系客服：',
+        r'短信验证码:\d+ 有效期5分钟.',
+        r'Account notification: The password for your (.*?) duyh[email protected] was recently changed',
+        r'Use\d+to verify your (.*?) account',
+        r'From:\s+(SoFi)',
+        r'\d+is your (.*?) OTP',
+        r'\d+ is your (.*?) OTP',
+        r'(.*?) Password Reset Code: \d+',
+        r'(.*?) 2FA Code: \d+\.',
+        r'(.*?) code: \d+. Do not share it or use it elsewhere!',
+        r'Hello \s+! Your (.*?) ride',
+        r'Please enter the below OTP code in the (.*?) to verify your phone number',
+        r'\d+ 是 (.*?)的验证码，15分钟内有效，仅用于登录，请勿告知他人。',
+        r'\d+是 (.*?)的验证码，15分钟内有效，仅用于登录，请勿告知他人。',
+        r'(\w+): Alert on Econoday event on',
+        r'(\w+): Your order is ready for pickup at',
+        r'(\w+): order canceled due to unusual account activity',
+        r'(\w+): DO NOT share this code',
+        r'Use\d+as (.*?) account security code',
+        r'Use \d+ as (.*?) account security code',
+        r'Use \d+ as (\w+) account password reset code',
+        r'Good news! A refund was issued for (\w+) order',
+        r'欢迎使用(.*?)，您的注册验证码为：',
+        
     ]
 
     # Create a dictionary to store company names and their counts
@@ -31,6 +58,16 @@ with open('sms_messages.txt', 'r') as infile, open('parsed_companies.txt', 'w') 
         for pattern in company_keywords:
             match = re.search(pattern, message, re.IGNORECASE)
             if match:
+                if "【阿里巴巴】您正在登录验证" in message:
+                    return "Alibaba"
+                elif "您正在进行短信登录" in message:
+                    return "Unamed Chinese Company"
+                elif "注册验证码" in message:
+                    return "Unamed Chinese Company"
+                elif "您本次验证码为" in message:
+                    return "Unamed Chinese Company"
+                elif "短信验证码" in message:
+                    return "Unamed Chinese Company"
                 return match.group(1)
         return None
 
@@ -44,7 +81,10 @@ with open('sms_messages.txt', 'r') as infile, open('parsed_companies.txt', 'w') 
         else:
             unparsed_messages.append(message)  # Store unparsed messages in a list
 
-    sorted_companies = sorted(company_counts.items(), key=lambda x: len(x[0]))
+    #sorted_companies = sorted(company_counts.items(), key=lambda x: len(x[0]))
+
+    # Sort the company names based on their counts
+    sorted_companies = sorted(company_counts.items(), key=lambda x: x[1], reverse=True)
 
     # Write the total parsed message count at the beginning of the output file
     outfile.write(f"Total Parsed Messages: {parsed_count}\n\n")
---
+++ pattern_finder.py
@@ -0,0 +1,39 @@
+import re
+import os
+from sklearn.cluster import KMeans
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Define the input file containing unparsed messages
+input_file = "unparsed_messages.txt"
+
+# Read and preprocess the messages
+messages = []
+with open(input_file, 'r') as file:
+    for line in file:
+        message = line.strip()
+        messages.append(message)
+
+# Use TF-IDF Vectorization to transform the text data into numerical features
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(messages)
+
+# Apply K-means clustering to categorize the messages into different groups
+num_clusters = 10  # Adjust this based on the number of clusters you want
+kmeans = KMeans(n_clusters=num_clusters, random_state=0)
+cluster_labels = kmeans.fit_predict(tfidf_matrix)
+
+# Create output directories for each cluster
+output_dir = "clustered_messages"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Store messages in separate files based on their clusters
+for cluster_num in range(num_clusters):
+    cluster_messages = [messages[i] for i, label in enumerate(cluster_labels) if label == cluster_num]
+    cluster_output_file = os.path.join(output_dir, f"cluster_{cluster_num}.txt")
+    
+    with open(cluster_output_file, 'w') as output_file:
+        for message in cluster_messages:
+            output_file.write(message + "\n")
+
+print("Messages have been clustered and stored in separate files based on their patterns.")
